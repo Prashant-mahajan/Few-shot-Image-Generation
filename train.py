e61a1913ef6990355845b6831e5f74c5b39f5c11
Prashant-mahajan <prashantmahajan70@gmail.com>
2019-04-15 14:32:29 -0400

    Modified code to print images and epochs

--- train.py
+++ train.py
@@ -13,7 +13,7 @@ options:
     -h, --help                  Show this help message and exit
 """
 from docopt import docopt
-
+import scipy.misc
 
 import torch
 import torch.optim as optim
@@ -70,7 +70,8 @@ class FIGR:
 
     def inner_loop(self, real_batch):
         self.meta_g.train()
-        fake_batch = self.meta_g(torch.tensor(np.random.normal(size=(self.batch_size, self.z_shape)), dtype=torch.float, device=device))
+        fake_batch = self.meta_g(
+            torch.tensor(np.random.normal(size=(self.batch_size, self.z_shape)), dtype=torch.float, device=device))
         training_batch = torch.cat([real_batch, fake_batch])
 
         # Training discriminator
@@ -84,7 +85,8 @@ class FIGR:
         self.meta_d_optim.step()
 
         # Training generator
-        output = self.meta_d(self.meta_g(torch.tensor(np.random.normal(size=(self.batch_size, self.z_shape)), dtype=torch.float, device=device)))
+        output = self.meta_d(self.meta_g(
+            torch.tensor(np.random.normal(size=(self.batch_size, self.z_shape)), dtype=torch.float, device=device)))
         generator_loss = wassertein_loss(output, self.generator_targets)
 
         self.meta_g_optim.zero_grad()
@@ -93,7 +95,7 @@ class FIGR:
 
         return discriminator_loss.item(), generator_loss.item()
 
-    def validation_run(self):
+    def validation_run(self, epoch_number):
         data, task = self.env.sample_validation_task(self.batch_size)
         training_images = data.cpu().numpy()
         training_images = np.concatenate([training_images[i] for i in range(self.batch_size)], axis=-1)
@@ -110,12 +112,21 @@ class FIGR:
 
         self.meta_g.eval()
         with torch.no_grad():
-            img = self.meta_g(torch.tensor(np.random.normal(size=(self.batch_size * 3, self.z_shape)), dtype=torch.float, device=device))
+            img = self.meta_g(
+                torch.tensor(np.random.normal(size=(self.batch_size * 3, self.z_shape)), dtype=torch.float,
+                             device=device))
         img = img.detach().cpu().numpy()
-        img = np.concatenate([np.concatenate([img[i * 3 + j] for j in range(3)], axis=-2) for i in range(self.batch_size)], axis=-1)
+        img = np.concatenate(
+            [np.concatenate([img[i * 3 + j] for j in range(3)], axis=-2) for i in range(self.batch_size)], axis=-1)
         img = unnormalize_data(img)
         img = np.concatenate([training_images, img], axis=-2)
-        self.writer.add_image('Validation_generated', img, self.eps)
+                
+        img = img[0, :, :]
+        
+        print("storing image at epoch", str(epoch_number))
+        scipy.misc.imsave('test/' + str(epoch_number) + '.png', img)
+        
+        # self.writer.add_image('Validation_generated', img, self.eps)
         self.writer.add_scalar('Validation_discriminator_loss', discriminator_total_loss, self.eps)
         self.writer.add_scalar('Validation_generator_loss', generator_total_loss, self.eps)
 
@@ -153,17 +164,17 @@ class FIGR:
         self.meta_g.load_state_dict(self.g.state_dict())
 
     def training(self):
-        while self.eps <= 1000000:
+        while self.eps <= 100000:
+            print("epoch number", self.eps)
             self.reset_meta_model()
             self.meta_training_loop()
 
             # Validation run every 10000 training loop
-            if self.eps % 10000 == 0:
+            if self.eps % 50 == 0:
                 self.reset_meta_model()
-                self.validation_run()
+                self.validation_run(self.eps)
                 self.checkpoint_model()
             self.eps += 1
-
 
     def load_args(self, args):
         self.outer_learning_rate = float(args['--outer_learning_rate'])
@@ -186,28 +197,32 @@ class FIGR:
 
     def get_id_string(self):
         return '{}_{}_olr{}_ilr{}_bsize{}_ie{}_h{}_l{}'.format(self.neural_network,
-                                                                         self.dataset,
-                                                                         str(self.outer_learning_rate),
-                                                                         str(self.inner_learning_rate),
-                                                                         str(self.batch_size),
-                                                                         str(self.inner_epochs),
-                                                                         str(self.height),
-                                                                         str(self.length))
+                                                               self.dataset,
+                                                               str(self.outer_learning_rate),
+                                                               str(self.inner_learning_rate),
+                                                               str(self.batch_size),
+                                                               str(self.inner_epochs),
+                                                               str(self.height),
+                                                               str(self.length))
 
     def initialize_gan(self):
         # D and G on CPU since they never do a feed forward operation
         self.d = eval(self.neural_network + 'Discriminator(self.env.channels, self.env.height, self.env.length)')
-        self.g = eval(self.neural_network + 'Generator(self.z_shape, self.env.channels, self.env.height, self.env.length)')
-        self.meta_d = eval(self.neural_network + 'Discriminator(self.env.channels, self.env.height, self.env.length)').to(device)
-        self.meta_g = eval(self.neural_network + 'Generator(self.z_shape, self.env.channels, self.env.height, self.env.length)').to(device)
+        self.g = eval(
+            self.neural_network + 'Generator(self.z_shape, self.env.channels, self.env.height, self.env.length)')
+        self.meta_d = eval(
+            self.neural_network + 'Discriminator(self.env.channels, self.env.height, self.env.length)').to(device)
+        self.meta_g = eval(
+            self.neural_network + 'Generator(self.z_shape, self.env.channels, self.env.height, self.env.length)').to(
+            device)
         self.d_optim = optim.Adam(params=self.d.parameters(), lr=self.outer_learning_rate)
         self.g_optim = optim.Adam(params=self.g.parameters(), lr=self.outer_learning_rate)
         self.meta_d_optim = optim.SGD(params=self.meta_d.parameters(), lr=self.inner_learning_rate)
         self.meta_g_optim = optim.SGD(params=self.meta_g.parameters(), lr=self.inner_learning_rate)
 
-        self.discriminator_targets = torch.tensor([1] * self.batch_size + [-1] * self.batch_size, dtype=torch.float, device=device).view(-1, 1)
+        self.discriminator_targets = torch.tensor([1] * self.batch_size + [-1] * self.batch_size, dtype=torch.float,
+                                                  device=device).view(-1, 1)
         self.generator_targets = torch.tensor([1] * self.batch_size, dtype=torch.float, device=device).view(-1, 1)
-
 
     def checkpoint_model(self):
         checkpoint = {'discriminator': self.d.state_dict(),
@@ -215,7 +230,9 @@ class FIGR:
                       'episode': self.eps}
         torch.save(checkpoint, 'Runs/' + self.id_string + '/checkpoint')
 
+
 if __name__ == '__main__':
+    print("************** start ****************")
     args = docopt(__doc__)
     env = FIGR(args)
     env.training()
